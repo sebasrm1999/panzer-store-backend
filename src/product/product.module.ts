1a315b709caf7df784af9867dc3d3b39cc303cef
Sebas Ramos <sebastian.ramed@gmail.com>
2023-01-25 16:03:00 -0600

    added axios and getting all products working

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,8 +1,10 @@
+import { HttpModule } from "@nestjs/axios";
 import { Module } from "@nestjs/common";
 import { ProductController } from "./product.controller";
 import { ProductService } from "./product.service";
 
 @Module({
+    imports: [HttpModule],
     controllers: [ProductController],
     providers: [ProductService],
 })
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,9 +1,25 @@
+import { HttpService } from "@nestjs/axios";
 import { Injectable } from "@nestjs/common";
+import { ForbiddenException } from "@nestjs/common/exceptions";
+import { Logger } from "@nestjs/common/services";
+import { catchError, firstValueFrom, map } from "rxjs";
 
 @Injectable({})
 export class ProductService {
-    products(){
-        return [{id : '1'}, {id : '2'}];
+    constructor(private httpService: HttpService){}
+    private url: string = 'https://fakestoreapi.com/';
+
+    async products(){
+        return this.httpService
+        .get(`${this.url}products?limit=5`, {headers: { "Accept-Encoding": "gzip,deflate,compress" }})
+        .pipe(
+            map(res => res.data),
+        )
+        .pipe(
+            catchError(() => {
+                throw new ForbiddenException('API not available');
+            }),
+        )
     }
 
     productsById(){
