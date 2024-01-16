82e9e9e341fd149e1d82aa9afde44e78c987f56a
sebasrm1999 <sebastian.ramed@gmail.com>
2024-01-15 18:13:38 -0600

    refactor: :recycle: Refactorized product service sorting method

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,36 +1,69 @@
-import { HttpService } from "@nestjs/axios";
-import { Injectable } from "@nestjs/common";
-import { ForbiddenException } from "@nestjs/common/exceptions";
-import { catchError, map } from "rxjs";
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { ForbiddenException } from '@nestjs/common/exceptions';
+import { catchError, map } from 'rxjs';
+
+function sortByOptions(options: string, firstProduct: any, secondProduct: any) {
+  if (options === 'desc') {
+    if (firstProduct.price < secondProduct.price) {
+      return 1;
+    }
+    if (firstProduct.price > secondProduct.price) {
+      return -1;
+    }
+    return 0;
+  }
+  if (options === 'asc') {
+    if (firstProduct.price > secondProduct.price) {
+      return 1;
+    }
+    if (firstProduct.price < secondProduct.price) {
+      return -1;
+    }
+    return 0;
+  }
+  return 0;
+}
 
 @Injectable({})
 export class ProductService {
-    constructor(private httpService: HttpService){}
-    private url: string = 'https://fakestoreapi.com/products';
+  constructor(private httpService: HttpService) {}
+  private url = 'https://fakestoreapi.com/products';
 
-    async products(){
-        return this.httpService
-        .get(`${this.url}?limit=5`, {headers: { "Accept-Encoding": "gzip,deflate,compress" }})
-        .pipe(
-            map(res => res.data),
-        )
-        .pipe(
-            catchError(() => {
-                throw new ForbiddenException('API not available');
-            }),
-        );
-    }
+  async products(options) {
+    return this.httpService
+      .get(`${this.url}`, {
+        headers: { 'Accept-Encoding': 'gzip,deflate,compress' },
+      })
+      .pipe(
+        map((res) => {
+          if (options && options !== '') {
+            const sortArray = res.data.sort((firstProduct, secondProduct) =>
+              sortByOptions(options, firstProduct, secondProduct),
+            );
+            return sortArray;
+          } else {
+            return res.data;
+          }
+        }),
+      )
+      .pipe(
+        catchError(() => {
+          throw new ForbiddenException('API not available');
+        }),
+      );
+  }
 
-    productsById(id: string){
-        return this.httpService
-        .get(`${this.url}/${id}`, {headers: { "Accept-Encoding": "gzip,deflate,compress" }})
-        .pipe(
-            map(res => res.data),
-        )
-        .pipe(
-            catchError(() => {
-                throw new ForbiddenException('API not available');
-            }),
-        );
-    }
+  productsById(id: string) {
+    return this.httpService
+      .get(`${this.url}/${id}`, {
+        headers: { 'Accept-Encoding': 'gzip,deflate,compress' },
+      })
+      .pipe(map((res) => res.data))
+      .pipe(
+        catchError(() => {
+          throw new ForbiddenException('API not available');
+        }),
+      );
+  }
 }
